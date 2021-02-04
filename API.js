00dacb0e0bb6252c28974ae25f9b808f4f2f44f6
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-05 02:22:27 +0600

    Search Member using API

--- API.js
+++ API.js
@@ -1,6 +1,7 @@
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(json => {
+        // displayUserName(json)
         if (json.length > 0) {
             let temp = ""
             json.forEach(elmt => {
@@ -11,11 +12,35 @@ fetch('https://jsonplaceholder.typicode.com/users')
                 temp += "<td>" + elmt.email + "</td>"
                 temp += "<td>" + elmt.address.street + ',' + elmt.address.suite + ',' + elmt.address.city + ',' + elmt.address.zipcode + ',' + 'Geoloc: [' + elmt.address.geo.lat + ',' + elmt.address.geo.lng + ']' + "</td>"
                 temp += "<td>" + elmt.phone + "</td>"
+                temp += "</tr>"
 
             });
             document.getElementById('data').innerHTML = temp
         }
     })
+
+document.getElementById('submit').addEventListener('click', function () {
+    const userName = document.getElementById('username').value;
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => {
+            let temp = ""
+            json.forEach(elmt => {
+                if (elmt.username == userName || elmt.name == userName) {
+                    temp += "<tr>"
+                    temp += "<td>" + elmt.id + "</td>"
+                    temp += "<td>" + elmt.name + "</td>"
+                    temp += "<td>" + elmt.username + "</td>"
+                    temp += "<td>" + elmt.email + "</td>"
+                    temp += "<td>" + elmt.address.street + ',' + elmt.address.suite + ',' + elmt.address.city + ',' + elmt.address.zipcode + ',' + 'Geoloc: [' + elmt.address.geo.lat + ',' + elmt.address.geo.lng + ']' + "</td>"
+                    temp += "<td>" + elmt.phone + "</td>"
+                    temp += "</tr>"
+                }
+            })
+            document.getElementById('data').innerHTML = temp
+        })
+})
+
 
 
 // function displayUserName(user) {
@@ -28,3 +53,15 @@ fetch('https://jsonplaceholder.typicode.com/users')
 //         ul.appendChild(li)
 //     }
 // }
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
