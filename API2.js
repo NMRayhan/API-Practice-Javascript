340f4be958b2ad0f346d26da2f4246e12aa46de3
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-06 21:18:20 +0600

    async and await keyword use

--- API2.js
+++ API2.js
@@ -11,5 +11,28 @@ document.getElementById('demo1').innerText = `
     ${today.name}
     ${today.today}
     ${today.city}
-    ${today.country}
-`
+    ${today.country}`
+
+
+async function LoadData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+LoadData();
+
+LoadData().then(data => {
+    displayData(data);
+})
+
+function displayData(data) {
+    const nodeList = window.document.getElementById('myList');
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        const list = document.createElement('li')
+        list.innerText = item.name;
+        nodeList.appendChild(list);
+    }
+}
